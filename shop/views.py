d14a07062fd364268789af95d233036935744a9b
erick <ericknicolas@gmail.com>
2020-05-16 14:23:46 +0200

    setun index content at home page with pagination

--- shop/views.py
+++ shop/views.py
@@ -7,5 +7,17 @@ from .models import Products
 class Home(generic.ListView):
     model = Products
     template_name = 'shop/home.html'
+    paginate_by = 8
 
+    def get_queryset(self):
+        try:
+            query = self.request.GET.get('q')
+        except:
+            query = ''
+        
+        if(query != '' and query is not None):
+            object_list = Products.objects.filter(title__icontains = query)
+        else:
+            object_list = Products.objects.all()
 
+        return object_list
